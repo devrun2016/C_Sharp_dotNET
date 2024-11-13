0da2803507b51c31bfe9e07a1dc2ff56f877f77c
Eric Kim <deveric89@naver.com>
2024-11-13 10:48:03 +0900

    MathMethods

---
+++ MathMethods/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        //Define Two Numbers
+        int numOne = 12932;
+        int numTwo = 2828472;
+
+        // Use built-in methods and save to variable 
+        double numberOneSqrt = Math.Floor(Math.Sqrt(Math.Abs(numOne)));
+
+        // Use built-in methods and save to variable 
+        double numberTwoSqrt = Math.Floor(Math.Sqrt(Math.Abs(numTwo)));
+
+        //Print
+        Console.WriteLine(Math.Min(numberOneSqrt, numberTwoSqrt));
+    }
+}
+
